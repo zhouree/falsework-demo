2d7d6d96e90769a675687cc533ea176fd405e12d
rui.zhou 周锐 <rui.zhou@andpay.me>
2015-11-10 18:47:35 +0800

    init demo

---
+++ src/main/java/net/falsework/demo/data/DemoEntity.java
@@ -0,0 +1,9 @@
+package net.falsework.demo.data;
+
+import net.falsework.data.orm.entity.IdEntity;
+
+public class DemoEntity extends IdEntity{
+
+	private static final long serialVersionUID = 1L;
+
+}
