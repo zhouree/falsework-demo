62171d953d1fbdbf6c7f87d904e244dc79399b87
rui.zhou 周锐 <rui.zhou@andpay.me>
2015-11-24 22:44:49 +0800

    add guava cache

--- src/main/java/net/falsework/demo/data/CityServiceImpl.java
+++ src/main/java/net/falsework/demo/data/CityServiceImpl.java
@@ -3,11 +3,14 @@ package net.falsework.demo.data;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@CacheConfig(cacheNames = "cityCache")
 public class CityServiceImpl implements CityService{
 	
 	@Autowired
@@ -23,6 +26,7 @@ public class CityServiceImpl implements CityService{
 	}
 	
 	@Override
+	@Cacheable
 	public List<City> query(City city) {
 		return cityMapper.query(city);
 	}
